b6b5775b17c2fd37eeb1f80e454583d82258eec6
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-06-05 19:47:38 +0700

    selesai membuat fitur card menu

--- vendor/js_content/index.js
+++ vendor/js_content/index.js
@@ -4,7 +4,7 @@
 
 const typetextspan = document.querySelector('.type-text');
 
-const textarray = ['Pizza?', 'Pasta?', 'Lasagna?', 'Drink?'];
+const textarray = ['Pizza?', 'Pasta?', 'Lasagna?', 'milkshake?'];
 
 const typingdelay = 120;
 const erasingdelay = 20;
@@ -45,92 +45,92 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 // code untuk mengambil data dari file JSON
-// 1.buat fungsi untuk ambil semua data pada menu
-function dataAllMenu(){
-    let menu_all  = fetch('pizza.json').then(e => {return e.json()}).then(el => {
-        let fragment_tag_menu_item = ``;
-        el.menu.forEach(function(edata){
-             fragment_tag_menu_item +=`
-            <div class="menu-item">
-                    <div class="gambar-pizza">
-                        <img src="vendor/img/pizza/${edata.gambar}" alt="" class="gambar-pizza-item">
-                    </div>
-                    <div class="desc-menu">
-                        <h4 class="nama-menu">
-                        ${edata.nama}
-                        </h4>
-                        <p class="deskripsi-menu-item">
-                        ${edata.deskripsi}
-                        </p>
-                        <p class="harga-menu-item">
-                        <span class="rp">Rp</span> ${edata.harga}
-                        </p>
+// // 1.buat fungsi untuk ambil semua data pada menu
+// function dataAllMenu(){
+//     let menu_all  = fetch('pizza.json').then(e => {return e.json()}).then(el => {
+//         let fragment_tag_menu_item = ``;
+//         el.menu.forEach(function(edata){
+//              fragment_tag_menu_item +=`
+//             <div class="menu-item">
+//                     <div class="gambar-pizza">
+//                         <img src="vendor/img/pizza/${edata.gambar}" alt="" class="gambar-pizza-item">
+//                     </div>
+//                     <div class="desc-menu">
+//                         <h4 class="nama-menu">
+//                         ${edata.nama}
+//                         </h4>
+//                         <p class="deskripsi-menu-item">
+//                         ${edata.deskripsi}
+//                         </p>
+//                         <p class="harga-menu-item">
+//                         <span class="rp">Rp</span> ${edata.harga}
+//                         </p>
                         
-                        <div class="btn-template-order">
-                        <button type="button" class="btn btn-order mt-1 text-capitalize">add to order</button>
-                        </div>
-                    </div>
-            </div>`;
+//                         <div class="btn-template-order">
+//                         <button type="button" class="btn btn-order mt-1 text-capitalize">add to order</button>
+//                         </div>
+//                     </div>
+//             </div>`;
            
-        })
-        menu_template.innerHTML = fragment_tag_menu_item;
-    })
-    return menu_all;
-}
-dataAllMenu()
+//         })
+//         menu_template.innerHTML = fragment_tag_menu_item;
+//     })
+//     return menu_all;
+// }
+// dataAllMenu()
 
-// 2.seleksi element container yaitu .menu-template 
-let menu_template = document.querySelector('.menu-template');
+// // 2.seleksi element container yaitu .menu-template 
+// let menu_template = document.querySelector('.menu-template');
 
-let nav_menu = document.querySelectorAll('.nav-menu');
+// let nav_menu = document.querySelectorAll('.nav-menu');
 
-// 3.lakukan looping untuk element dengan class .nav-menu
-nav_menu.forEach(function(el){
-    el.addEventListener('click',function(e){
-        e.preventDefault()
-        let target = e.target.textContent;
+// // 3.lakukan looping untuk element dengan class .nav-menu
+// nav_menu.forEach(function(el){
+//     el.addEventListener('click',function(e){
+//         e.preventDefault()
+//         let target = e.target.textContent;
     
-        // 4.ambil data dari json menggunakan fetch
-            fetch('pizza.json')
-            .then(e => {return e.json()})
-            .then(function(e) {
-                   let obj_menu = e.menu;
-                   let fragment_tag_menu_item = ``
+//         // 4.ambil data dari json menggunakan fetch
+//             fetch('pizza.json')
+//             .then(e => {return e.json()})
+//             .then(function(e) {
+//                    let obj_menu = e.menu;
+//                    let fragment_tag_menu_item = ``
                        
-                //   5.looping data pada json
-                    obj_menu.forEach(element => {
-                        // 6.cek apakah nav menu yang di klik sama dengan katergi data json
-                            // 7.jika target.textContent ==== property kategory
-                        if(target === element.kategori){
+//                 //   5.looping data pada json
+//                     obj_menu.forEach(element => {
+//                         // 6.cek apakah nav menu yang di klik sama dengan katergi data json
+//                             // 7.jika target.textContent ==== property kategory
+//                         if(target === element.kategori){
                            
-                            fragment_tag_menu_item += `
-                             <div class="menu-item">
-                                    <div class="gambar-pizza">
-                                        <img src="vendor/img/pizza/${element.gambar}" alt="" class="gambar-pizza-item">
-                                    </div>
-                                    <div class="desc-menu">
-                                        <h4 class="nama-menu">
-                                       ${element.nama}
-                                        </h4>
-                                        <p class="deskripsi-menu-item">
-                                        ${element.deskripsi}
-                                        </p>
-                                        <p class="harga-menu-item">
-                                        <span class="rp">Rp</span> ${element.harga}
-                                        </p>
+//                             fragment_tag_menu_item += `
+//                              <div class="menu-item">
+//                                     <div class="gambar-pizza">
+//                                         <img src="vendor/img/pizza/${element.gambar}" alt="" class="gambar-pizza-item">
+//                                     </div>
+//                                     <div class="desc-menu">
+//                                         <h4 class="nama-menu">
+//                                        ${element.nama}
+//                                         </h4>
+//                                         <p class="deskripsi-menu-item">
+//                                         ${element.deskripsi}
+//                                         </p>
+//                                         <p class="harga-menu-item">
+//                                         <span class="rp">Rp</span> ${element.harga}
+//                                         </p>
                                         
-                                        <div class="btn-template-order">
-                                        <button type="button" class="btn btn-order mt-1 text-capitalize">add to order</button>
-                                        </div>
-                                     </div>
-                            </div>`;
-                            menu_template.innerHTML = fragment_tag_menu_item;
-                        }
-                        // 8.cek jika user memilih / mengclick  kategori all menu di nav menu
-                         if(target === 'all menu'){
-                            dataAllMenu()
-                        }
-                    });
-            })
-    })
-})
+//                                         <div class="btn-template-order">
+//                                         <button type="button" class="btn btn-order mt-1 text-capitalize">add to order</button>
+//                                         </div>
+//                                      </div>
+//                             </div>`;
+//                             menu_template.innerHTML = fragment_tag_menu_item;
+//                         }
+//                         // 8.cek jika user memilih / mengclick  kategori all menu di nav menu
+//                          if(target === 'all menu'){
+//                             dataAllMenu()
+//                         }
+//                     });
+//             })
+//     })
+// })
